c50d8a7e8a2d29ffd2ec0518d5b89354e5fd3859
Dani Pérez <daniel.perez.partner@decathlon.com>
2023-05-15 10:03:22 +0200

    Add first migration version

---
+++ migrations/Version20230514170138.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230514170138 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create tables and its relationships';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            <<<SQL
+                CREATE TABLE `laperillatxt_database`.`txt` (
+                    `id` CHAR(36) PRIMARY KEY NOT NULL,
+                    `title` VARCHAR(100) NOT NULL,
+                    `text` VARCHAR(500) NOT NULL,
+                    INDEX IDX_txt_title (`title`)
+                );
+            SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(
+            <<<SQL
+                DROP TABLE `laperillatxt_database`.`txt`;
+            SQL
+        );
+    }
+}
