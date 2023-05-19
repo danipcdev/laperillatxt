4ec50dddde7f5322130f42013f98e926fffb7735
Dani Pérez <daniel.perez.partner@decathlon.com>
2023-05-19 08:52:20 +0200

    Add asserts for getTxtById

--- tests/Functional/Txt/Controller/Txt/CreateTxtControllerTest.php
+++ tests/Functional/Txt/Controller/Txt/CreateTxtControllerTest.php
@@ -27,5 +27,22 @@ class CreateTxtControllerTest extends TxtControllerTestBase
         self::assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
         self::assertArrayHasKey('txtId', $responseData);
         self::assertEquals(36, \strlen($responseData['txtId']));
+
+        $generatedTxtId = $responseData['txtId'];
+
+        self::$client->request(Request::METHOD_GET, \sprintf('/txt/%s', $generatedTxtId ));
+
+        $response = self::$client->getResponse();
+        $responseData = $this->getResponseData($response);
+
+        self::assertEquals(Response::HTTP_OK, $response->getStatusCode());
+
+        self::assertArrayHasKey('id', $responseData);
+        self::assertArrayHasKey('title', $responseData);
+        self::assertArrayHasKey('text', $responseData);
+
+        self::assertEquals($generatedTxtId, $responseData['id']);
+        self::assertEquals($payload['title'], $responseData['title']);
+        self::assertEquals($payload['text'], $responseData['text']);
     }
 }
